bd2dd65521d230ea5c2970b89b64fcfb04b4cb3f
Niklas Fritsch <ev-m-nf@Niklass-MBP.fritz.box>
2022-07-04 08:42:02 +0200

    hmm yea

--- cmd/ytrc/main.go
+++ cmd/ytrc/main.go
@@ -5,88 +5,13 @@ import (
 	"net/http"
 	kvdb "nifri2/ytrc/db"
 	funcs "nifri2/ytrc/handlers"
-	"os"
-	"runtime"
-	"strings"
 
 	"github.com/gorilla/mux"
 	"github.com/rs/zerolog/log"
 )
 
-func find_bin() string {
-	var bin string
-	var found bool
-	var sep string
-	var psep string
-
-	env_path := os.Getenv("PATH")
-	os_v := runtime.GOOS
-
-	switch os_v {
-	case "windows":
-		log.Info().Msg("OS: Windows")
-		sep = ";"
-		psep = "\\"
-	case "darwin":
-		log.Info().Msg("OS: Mac")
-		sep = ":"
-		psep = "/"
-	case "linux":
-		log.Info().Msg("OS: Linux")
-		sep = ":"
-		psep = "/"
-	default:
-		log.Warn().Msg("OS: Unknown")
-		sep = ":"
-		psep = "/"
-	}
-
-	for _, path := range strings.Split(env_path, sep) {
-		log.Info().Str("path", path).Msg("Checking path")
-
-		p, err := os.ReadDir(path)
-		if err != nil {
-			log.Error().Err(err).Msg("Error reading path")
-			continue
-		}
-
-		for _, file := range p {
-			switch file.Name() {
-			case "yt-dlp":
-				bin = path + psep + file.Name()
-				log.Info().Str("bin", bin).Msg("Found yt-dlp")
-				found = true
-			case "yt-dlp.exe":
-				bin = path + psep + file.Name()
-				log.Info().Str("bin", bin).Msg("Found yt-dlp.exe")
-				found = true
-			case "yt-dlp_macos":
-				bin = path + psep + file.Name()
-				log.Info().Str("bin", bin).Msg("Found yt-dlp_macos")
-				found = true
-			case "yt-dlp_linux":
-				bin = path + psep + file.Name()
-				log.Info().Str("bin", bin).Msg("Found yt-dlp_linux")
-				found = true
-			}
-
-			if found {
-				return bin
-			}
-		}
-
-	}
-
-	if !found {
-		log.Error().Str("file", "yt-dlp").Msg("Not found, please install yt-dlp")
-	}
-
-	bin = "not found"
-	return bin
-}
-
 func main() {
-	bin := find_bin()
+	bin := funcs.Find_bin()
 	if bin == "not found" {
 		panic("yt-dlp not found in $PATH")
 	}
--- handlers/funcs.go
+++ handlers/funcs.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"net/http"
 	kvdb "nifri2/ytrc/db"
+	"os"
+	"runtime"
+	"strings"
 	"time"
 
 	"github.com/go-cmd/cmd"
@@ -61,4 +64,76 @@ func Grabber(url string) string {
 			continue
 		}
 	}
+}
+
+func Find_bin() string {
+	var bin string
+	var found bool
+	var sep string
+	var psep string
+
+	env_path := os.Getenv("PATH")
+	os_v := runtime.GOOS
+
+	switch os_v {
+	case "windows":
+		log.Info().Msg("OS: Windows")
+		sep = ";"
+		psep = "\\"
+	case "darwin":
+		log.Info().Msg("OS: Mac")
+		sep = ":"
+		psep = "/"
+	case "linux":
+		log.Info().Msg("OS: Linux")
+		sep = ":"
+		psep = "/"
+	default:
+		log.Warn().Msg("OS: Unknown")
+		sep = ":"
+		psep = "/"
+	}
+
+	for _, path := range strings.Split(env_path, sep) {
+		log.Info().Str("path", path).Msg("Checking path")
+
+		p, err := os.ReadDir(path)
+		if err != nil {
+			log.Error().Err(err).Msg("Error reading path")
+			continue
+		}
+
+		for _, file := range p {
+			switch file.Name() {
+			case "yt-dlp":
+				bin = path + psep + file.Name()
+				log.Info().Str("bin", bin).Msg("Found yt-dlp")
+				found = true
+			case "yt-dlp.exe":
+				bin = path + psep + file.Name()
+				log.Info().Str("bin", bin).Msg("Found yt-dlp.exe")
+				found = true
+			case "yt-dlp_macos":
+				bin = path + psep + file.Name()
+				log.Info().Str("bin", bin).Msg("Found yt-dlp_macos")
+				found = true
+			case "yt-dlp_linux":
+				bin = path + psep + file.Name()
+				log.Info().Str("bin", bin).Msg("Found yt-dlp_linux")
+				found = true
+			}
+
+			if found {
+				return bin
+			}
+		}
+
+	}
+
+	if !found {
+		log.Error().Str("file", "yt-dlp").Msg("Not found, please install yt-dlp")
+	}
+
+	bin = "not found"
+	return bin
 }
